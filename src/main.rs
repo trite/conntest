7fe4d3900206e2507a4e5a987c2f8a595f55acc8
trite <trite@users.noreply.github.com>
2022-03-28 08:46:55 -0700

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -66,39 +66,4 @@ fn main() -> Result<()>{
     Ok(())
 }
 
-// TODO: UI work and improvements?
-// Current state:
-/*
-    PS C:\git\conntest> cargo run -- localhost,google.com,blorg.badtld 80,443 -v
-    Compiling conntest v0.1.0 (C:\git\conntest)
-        Finished dev [unoptimized + debuginfo] target(s) in 1.13s
-        Running `target\debug\conntest.exe localhost,google.com,blorg.badtld 80,443 -v`
-    ====== Details ======
-    to_scan:
-        localhost([::1]:80)
-        localhost([::1]:443)
-        google.com(172.217.14.110:80) 
-        google.com(172.217.14.110:443)
-
-    cannot_scan:
-        blorg.badtld:80
-        blorg.badtld:443
-
-    timeout: None
-    delay: None
-    ====== Output ======
-    blorg.badtld:80 cannot be scanned
-    blorg.badtld:443 cannot be scanned
-    localhost([::1]:80) is open
-    localhost([::1]:443) is closed
-    google.com(172.217.14.110:80) is open
-    google.com(172.217.14.110:443) is open
-
-    localhost([::1]:80) is open
-    localhost([::1]:443) is closed
-    google.com(172.217.14.110:80) is open
-    google.com(172.217.14.110:443) is open
-
-    error: process didn't exit successfully: `target\debug\conntest.exe localhost,google.com,blorg.badtld 80,443 -v` (exit code: 0xc000013a, STATUS_CONTROL_C_EXIT)
-    PS C:\git\conntest> 
-*/
+// TODO: Async and UI
