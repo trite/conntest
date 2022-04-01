a5d2cec05546a64bc709adf5250aef95d26a658b
trite <trite@users.noreply.github.com>
2022-04-01 09:10:08 -0700

    Sparklines are showing now, but not correctly...

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,16 @@ use options::{Options, Defaults, HostInfo};
 use tui::{
     backend::CrosstermBackend,
     Terminal,
-    layout::{Layout, Direction, Constraint}
+    layout::{Layout, Direction, Constraint}, widgets::{Sparkline, Block, Borders}, style::{Style, Color}
 };
 
 use std::{
     io,
     net::TcpStream,
     sync::mpsc,
-    collections::HashMap,
+    collections::BTreeMap,
     thread::{self, sleep},
-    time::{Duration, Instant},
+    time::{Duration, Instant}, process::exit,
 };
 
 enum Event<I> {
@@ -41,14 +41,14 @@ enum ConnectivityUpdate {
 }
 
 struct State {
-    history: HashMap<String, Vec<Connectivity>>,
+    history: BTreeMap<String, Vec<Connectivity>>,
 }
 
 fn main() -> Result<()>{
     enable_raw_mode().expect("can run in raw mode");
 
-    let (cmdTx, cmdRx) = mpsc::channel();
-    let (updateTx, updateRx) = mpsc::channel();
+    let (cmd_tx, cmd_rx) = mpsc::channel();
+    let (update_tx, update_rx) = mpsc::channel();
     let tick_rate = Duration::from_millis(200);
 
     thread::spawn(move || {
@@ -60,12 +60,12 @@ fn main() -> Result<()>{
 
             if event::poll(timeout).expect("poll works") {
                 if let CEvent::Key(key) = event::read().expect("can read events") {
-                    cmdTx.send(Event::Input(key)).expect("can send events");
+                    cmd_tx.send(Event::Input(key)).expect("can send events");
                 }
             }
 
             if last_tick.elapsed() >= tick_rate {
-                if let Ok(_) = cmdTx.send(Event::Tick) {
+                if let Ok(_) = cmd_tx.send(Event::Tick) {
                     last_tick = Instant::now();
                 }
             }
@@ -82,26 +82,26 @@ fn main() -> Result<()>{
         delay: Duration::from_secs(5),
     })?;
 
-    if options.verbose {
-        println!("====== Details ======");
-        println!("  to_scan:");
-        for host in &options.to_scan {
-            println!("    {}", host);
-        }
-        println!("\n  cannot_scan:");
-        for host in &options.cannot_scan {
-            println!("    {}", host);
-        }
-        println!("\n  timeout: {:?}", options.timeout);
-        println!("  delay: {:?}", options.delay);
-        println!("====== Output ======");
-    }
+    // if options.verbose {
+    //     println!("====== Details ======");
+    //     println!("  to_scan:");
+    //     for host in &options.to_scan {
+    //         println!("    {}", host);
+    //     }
+    //     println!("\n  cannot_scan:");
+    //     for host in &options.cannot_scan {
+    //         println!("    {}", host);
+    //     }
+    //     println!("\n  timeout: {:?}", options.timeout);
+    //     println!("  delay: {:?}", options.delay);
+    //     println!("====== Output ======");
+    // }
 
-    for fail in options.cannot_scan {
-        println!("{} cannot be scanned", fail);
-    }
+    // for fail in options.cannot_scan {
+    //     println!("{} cannot be scanned", fail);
+    // }
 
-    println!("\nCTRL-C to break\n");
+    // println!("\nCTRL-C to break\n");
 
     let mut last_run = Instant::now().checked_sub(options.delay).unwrap();
 
@@ -113,8 +113,8 @@ fn main() -> Result<()>{
             if last_run.elapsed() >= options.delay {
                 for info in &options.to_scan {
                     match TcpStream::connect_timeout(&info.addr, options.timeout) {
-                        Ok(_) => updateTx.send(ConnectivityUpdate::Success(info.clone())).unwrap(),
-                        Err(_) => updateTx.send(ConnectivityUpdate::Failure(info.clone())).unwrap()
+                        Ok(_) => update_tx.send(ConnectivityUpdate::Success(info.clone())).unwrap(),
+                        Err(_) => update_tx.send(ConnectivityUpdate::Failure(info.clone())).unwrap()
                     }
                 }
                 last_run = Instant::now();
@@ -123,43 +123,12 @@ fn main() -> Result<()>{
         }
     });
 
+    let mut state = State {
+        history: BTreeMap::new()
+    };
 
     loop {
-        let mut state = State {
-            history: HashMap::new()
-        };
-
-        terminal.draw(|rect| {
-            let size = rect.size();
-            let mut constraints = vec![];
-            for _ in 0..state.history.len() {
-                constraints.push(Constraint::Length(2));
-            }
-            // let constraints: Vec<Constraint> = options.to_scan.clone().iter().map(|h| Constraint::Length(2)).collect::<Vec<Constraint>>().clone();
-            let chunks = Layout::default()
-                .direction(Direction::Vertical)
-                .margin(1)
-                .constraints(constraints)
-                .split(size);
-
-            // TODO: Draw out the sparklines for each history
-            // (might need to use a running index while inserting into constraint sections)
-            println!("{:?}", state.history);
-        });
-
-        match cmdRx.recv()? {
-            Event::Input(evt) => match evt.code {
-                KeyCode::Char('q') => {
-                    disable_raw_mode()?;
-                    terminal.show_cursor()?;
-                    break;
-                }
-                _ => {}
-            },
-            Event::Tick => { }
-        };
-
-        match updateRx.recv()? {
+        match update_rx.recv()? {
             ConnectivityUpdate::Success(host) => {
                 state.history
                     .entry(host.to_string())
@@ -173,6 +142,74 @@ fn main() -> Result<()>{
                     .push(Connectivity::Failure);
             },
         };
+        terminal.draw(|rect| {
+            let size = rect.size();
+            let mut constraints = vec![];
+            let pct = 100 / state.history.len() as u16;
+            for _ in 0..state.history.len() {
+                constraints.push(Constraint::Percentage(pct));
+            }
+            // let constraints: Vec<Constraint> = options.to_scan.clone().iter().map(|h| Constraint::Length(2)).collect::<Vec<Constraint>>().clone();
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints(constraints)
+                .split(size);
+            
+            let mut i = 0;
+            for (host, connectivity) in &state.history {
+                let data =
+                    &connectivity.iter().map(|c| match c {
+                            Connectivity::Failure => 50u64,
+                            Connectivity::Success => 100u64
+                    })
+                    .collect::<Vec<u64>>();
+                let sparkline = Sparkline::default()
+                    .block(
+                        Block::default()
+                            .title(host.clone())
+                            .borders(Borders::LEFT | Borders::RIGHT),
+                    )
+                    .data(data)
+                    .style(
+                        Style::default()
+                            .fg(Color::Red),
+                    );
+                rect.render_widget(sparkline, chunks[i]);
+                i += 1;
+            }
+            // TODO: Draw out the sparklines for each history
+            // (might need to use a running index while inserting into constraint sections)
+        })?;
+
+        match cmd_rx.recv()? {
+            Event::Input(evt) => match evt.code {
+                KeyCode::Char('q') => {
+                    disable_raw_mode()?;
+                    terminal.show_cursor()?;
+                    exit(0);
+                }
+                _ => {}
+            },
+            Event::Tick => { }
+        };
+
+
+        // let timeout = tick_rate
+        //     .checked_sub(last_tick.elapsed())
+        //     .unwrap_or_else(|| Duration::from_secs(0));
+
+        // if event::poll(timeout).expect("poll works") {
+        //     if let CEvent::Key(key) = event::read().expect("can read events") {
+        //         cmd_tx.send(Event::Input(key)).expect("can send events");
+        //     }
+        // }
+
+        // if last_tick.elapsed() >= tick_rate {
+        //     if let Ok(_) = cmd_tx.send(Event::Tick) {
+        //         last_tick = Instant::now();
+        //     }
+        // }
     }
 
     Ok(())
